188bd062a5d341a3cd672d6d2ff7e9b54861b70f
Juan Estrada <juanestrada@Juans-MacBook-Air.local>
2025-12-14 16:02:16 -0500

    all by myself

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document.getElementById('btn').onclick = function(){
-    alert('Button clicked!');
-    console.log("Hello, World!");
-    console.log("another one!");
-};
+const button = document.getElementById('writeBtn');
+const output = document.getElementById('output');
 
+button.addEventListener('click', () =>{
+    output.innerText = "TA-DA!";
+    console.log("What is up dawg!");
+});
