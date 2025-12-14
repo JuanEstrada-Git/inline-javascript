5a1fb9bdbac972d3f7b8e73be9f309100f2342ef
Juan Estrada <juanestrada@Juans-MacBook-Air.local>
2025-12-14 16:12:19 -0500

    did it again by myself

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const button = document.getElementById('writeBtn');
+const button = document.getElementById('rewrite');
 const output = document.getElementById('output');
 
-button.addEventListener('click', () =>{
-    output.innerText = "TA-DA!";
-    console.log("What is up dawg!");
+button.addEventListener('click', () => {
+    output.innerText = "Replaced dawg!";
+    console.log('Replaced dawg!');
 });
